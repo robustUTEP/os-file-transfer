a8809d7f0409bc8ba29c09a09554859e34e835f0
Eric Freudenthal <eric@freudenthal.net>
2023-10-29 21:37:11 -0600

    cleaned up, commented

--- fork-demo/helloServer.py
+++ fork-demo/helloServer.py
@@ -12,31 +12,17 @@ switchesVarDefaults = (
     )
 
 
-
-progname = "echoserver"
 paramMap = params.parseParams(switchesVarDefaults)
 
 listenPort = paramMap['listenPort']
 listenAddr = ''       # Symbolic name meaning all available interfaces
 
-numChildren = 0
-pidAddr = {}
+pidAddr = {}                    # for active connections: maps pid->client addr 
 
 if paramMap['usage']:
     params.usage()
 
-servSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# socket will unbind immediately on close
-servSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# accept will timeout in 5s
-servSock.settimeout(5)          
-
-servSock.bind((listenAddr, listenPort))
-servSock.listen(1)              # allow only one outstanding request
-# s is a factory for connected sockets
-
-# run by child
+# server code to be run by child
 def chatWithClient(connAddr):  
     sock, addr = connAddr
     print(f'Child: pid={os.getpid()} connected to client at {addr}')
@@ -46,22 +32,35 @@ def chatWithClient(connAddr):
     sock.shutdown(socket.SHUT_WR)
     sys.exit(0)                 # terminate child
 
+listenSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# listener socket will unbind immediately on close
+listenSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# accept will block for no more than 5s
+listenSock.settimeout(5)          
+# bind listener socket to port
+listenSock.bind((listenAddr, listenPort))
+# set state to listen
+listenSock.listen(1)              # allow only one outstanding request
+
+# s is a factory for connected sockets
+
+
 while True:
     # reap zombie children (if any)
     while pidAddr.keys():
+        # Check for exited children (zombies).  If none, don't block (hang)
         if (waitResult := os.waitid(os.P_ALL, 0, os.WNOHANG | os.WEXITED)): 
             zPid, zStatus = waitResult.si_pid, waitResult.si_status
             print(f"""zombie reaped:
             \tpid={zPid}, status={zStatus}
             \twas connected to {pidAddr[zPid]}""")
             del pidAddr[zPid]
-            numChildren -= 1
         else:
             break               # no zombies; break from loop
     print(f"Currently {len(pidAddr.keys())} clients")
 
     try:
-        connSockAddr = servSock.accept() # accept connection from a new client
+        connSockAddr = listenSock.accept() # accept connection from a new client
     except TimeoutError:
         connSockAddr = None 
 
@@ -70,10 +69,9 @@ while True:
         
     forkResult = os.fork()     # fork child for this client 
     if (forkResult == 0):        # child
-        servSock.close()         # child doesn't need servSock
+        listenSock.close()         # child doesn't need listenSock
         chatWithClient(connSockAddr)
     # parent
-    numChildren += 1
     sock, addr = connSockAddr
     sock.close()   # parent closes its connection to client
     pidAddr[forkResult] = addr
